4fd098c6ac4ec147ca07b07df6ce57ce1654baef
Fernando Ibarra <Fernandooibarra@gmail.com>
2025-10-04 18:16:58 -0700

    fix: problems

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -2,7 +2,7 @@
 import { createBrowserRouter } from "react-router"
 import { Layout } from '../layout/Layout';
 import { HomePage } from "@/home/pages/HomePage";
-import { ProductsPage } from "@/products/pages/ProductsPage";
+// import { ProductsPage } from "@/products/pages/ProductsPage";
 import { InfoPage } from "@/info/pages/InfoPage";
 import { BranchesPage } from "@/branches/pages/BranchesPage";
 
