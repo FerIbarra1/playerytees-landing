7fd9a03723a2d92f4aa273aecfd0f8e5607ecc26
Fernando Ibarra <Fernandooibarra@gmail.com>
2025-10-05 14:43:43 -0700

    fix: icon map

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { AppShop } from './AppShop'
+
+import './index.css'
+import "./leaflet-fix-default-icon";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
